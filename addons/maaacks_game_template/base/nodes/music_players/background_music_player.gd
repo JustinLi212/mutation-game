7eb8af42e0809d9448cd215f7b84e9a610730d9c
Justin Li <jdli3@uci.edu>
2026-06-16 11:16:33 -0700

    Pause music start

--- addons/maaacks_game_template/base/nodes/music_players/background_music_player.gd
+++ addons/maaacks_game_template/base/nodes/music_players/background_music_player.gd
@@ -1,11 +1,40 @@
 extends AudioStreamPlayer
 
+signal game_paused
+signal game_unpaused
 signal music_looped
 
-var last_playback_pos: float = 0.0
+@onready var music_timer: Timer = $MusicTimer
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if get_playback_position() < last_playback_pos:
-		music_looped.emit()
-	last_playback_pos = get_playback_position()
+var stream_playback: AudioStreamPlaybackInteractive
+
+
+func _ready() -> void:
+	music_timer.start()
+	music_timer.timeout.connect(music_looped.emit)
+	music_timer.timeout.connect(_on_music_timeout)
+	game_paused.connect(_on_game_paused)
+	game_unpaused.connect(_on_game_unpaused)
+	stream_playback = get_stream_playback() as AudioStreamPlaybackInteractive
+
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_PAUSED:
+		game_paused.emit()
+	elif what == NOTIFICATION_UNPAUSED:
+		game_unpaused.emit()
+
+
+func _on_music_timeout() -> void:
+	print("HI")
+
+
+func _on_game_paused() -> void:
+	print("HI")
+	stream_playback.switch_to_clip_by_name(&"pause")
+	music_timer.paused = true
+
+
+func _on_game_unpaused() -> void:
+	stream_playback.switch_to_clip_by_name(&"game")
+	music_timer.paused = false
--- scenes/game_scene/grid/grid.gd
+++ scenes/game_scene/grid/grid.gd
@@ -6,6 +6,7 @@ extends AnimatableBody2D
 var is_active: bool = false
 var shake_tween: Tween
 var SHAKE_AMOUNT: float = 5.0
+var SHAKE_TIME: float = 0.1
 var base_pos: Vector2
 
 @onready var labels: Node2D = $GridLabels
@@ -23,10 +24,10 @@ func _ready() -> void:
 func _on_grid_shaken(dir: Vector2) -> void:
 	# Don't shake if already shaking
 	if shake_tween and shake_tween.is_running():
-		return
+		shake_tween.kill()
 	shake_tween = create_tween()
-	shake_tween.tween_property(self, "global_position", dir * SHAKE_AMOUNT, 0.1).as_relative()
-	shake_tween.tween_property(self, "global_position", base_pos, 0.1)
+	shake_tween.tween_property(self, "global_position", base_pos + dir * SHAKE_AMOUNT, SHAKE_TIME)
+	shake_tween.tween_property(self, "global_position", base_pos, SHAKE_TIME)
 
 
 func _on_animated_sprite_2d_frame_changed() -> void:
